02bcfe2fb0ff871f7c1e4dac80e3d8e47edea1c3
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2024-03-30 15:36:15 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/arjkb/jiralog
+
+go 1.22.1
---
+++ jiralog.go
@@ -0,0 +1,11 @@
+// Program to log time entries to Jira.
+// Author: Arjun Krishna Babu
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("jiralog")
+}
