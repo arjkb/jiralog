a9cc4a296f3abe1824c2cc1bf3ff78093e58a963
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2024-03-31 12:10:11 +0530

    implement func to read time, along with tests

--- jiralog.go
+++ jiralog.go
@@ -4,8 +4,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("jiralog")
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+	for i := 0; i < len(lines)-1; i++ {
+		startTime, err := readTime(lines[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		endTime, err := readTime(lines[i+1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%d-%d: %s\n", startTime, endTime, lines[i])
+	}
+}
+
+// readTime returns the time portion from a string.
+func readTime(line string) (int, error) {
+	num, err := strconv.Atoi(line[:4])
+	if num < 0 {
+		return -1, fmt.Errorf("negative time read: %d", num)
+	}
+
+	minute := num % 100
+	if minute > 59 {
+		return -1, fmt.Errorf("minute greater than 59: %d", minute)
+	}
+
+	return num, err
 }
---
+++ jiralog_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+)
+
+// TestReadTimeWithValidInputs tests the readTime() function with several
+// different valid inputs.
+func TestReadTimeWithValidInputs(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{"0123: test case 1", 123},
+		{"0900: test case 2", 900},
+		{"1234: test case 2", 1234},
+		{"1350: test case 2", 1350},
+		{"0000: test case with all zero time", 0},
+	}
+
+	for _, test := range tests {
+		got, err := readTime(test.input)
+		if got != test.want || err != nil {
+			t.Errorf("readTime(%q) = %d, %v, want %d", test.input, got, err, test.want)
+		}
+	}
+}
+
+// TestReadTimeWithInvalidInputs rests the readTime() function with several
+// different invalid inputs.
+func TestReadTimeWithInvalidInputs(t *testing.T) {
+	var tests = []string{
+		"1270: invlid minutes",
+		"without time",
+		"-124: negative time",
+	}
+
+	for _, input := range tests {
+		got, err := readTime(input)
+		if err == nil {
+			t.Errorf("readTime(%q) = %d, %v", input, got, err)
+		}
+	}
+}
