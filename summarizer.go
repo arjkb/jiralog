74cc610cf1e861fc080ac674eb5d07dc47249eff
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2026-06-21 16:45:09 +0530

    define method to get provider

--- summarizer.go
+++ summarizer.go
@@ -12,6 +12,22 @@ type Summarizer interface {
 	Summarize(ctx context.Context, query string) (string, error)
 }
 
+func getProvider(config Config) (Summarizer, error) {
+	if config.Aikey == "" {
+		return nil, fmt.Errorf("missing key; check config file")
+	}
+	if config.Model == "" {
+		return nil, fmt.Errorf("missing model; check config file")
+	}
+
+	switch config.Provider {
+	case "openai":
+		return &OpenAIProvider{key: config.Aikey, model: config.Model}, nil
+	}
+
+	return nil, fmt.Errorf("invalid provider; check config file")
+}
+
 // Get the worklog summary
 func getWorklogSummary(ctx context.Context, key string, model string, prompt string, rawDescription string) (string, error) {
 	if key == "" {
